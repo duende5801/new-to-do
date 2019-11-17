c06f19fd5df58f6d575444f475cf0055413255cf
duende5801 <duende5801@gmail.com>
2019-11-17 15:04:31 -0800

    created a tabable list group item

--- js/script.js
+++ js/script.js
@@ -1,29 +1,33 @@
 //---------------grab all elements------------//
 
-//addToList, addElement, addList, deleteItem, editItem, inputText
+//addToList, addListItem, addNewList, addList, deleteItem, editItem, inputText
 
 let inputText = document.getElementById('inputText');
-let addElement = document.getElementById('addElement');
-let addToList = document.getElementById('addToList');
-let addList = document.getElementById('addList');
-let deleteItem = document.getElementById('deleteItem');
-let editItem = document.getElementById('editItem');
+let addToListBtn = document.getElementById('addToList');
+let addListBtn = document.getElementById('addList');
+let deleteItemBtn = document.getElementById('deleteItem');
+let editItemBtn = document.getElementById('editItem');
+//html append id
+let addNewList = document.getElementById('addNewList');
+let addListItem = document.getElementById('addListItem')
 
 
 //-------------add event listeners----------//
-addList.addEventListener('click', function(e){
-    let ulElement = document.createElement('ul');
-    addElement.append(ulElement);
+addListBtn.addEventListener('click', function(e){
+    let aElement = document.createElement('a');
+    aElement.setAttribute('class', 'list-group-item list-group-item-action');
+    aElement.setAttribute('id', inputText.value + '-list');
+    aElement.setAttribute('data-toggle', 'list');
+    aElement.setAttribute('href', '#');
+    aElement.setAttribute('role', 'tab');
+    addNewList.append(aElement);
 })
-addToList.addEventListener('click', function(e){
-    let liElement = document.createElement('li');
-    liElement.setAttribute('class', 'list-group-item')
-    liElement.innerText = inputText.value;
-    addElement.append(liElement)
-})
-deleteItem.addEventListener('click', function(e){
+addToListBtn.addEventListener('click', function(e){
 
 })
-editItem.addEventListener('click', function(e){
+deleteItemBtn.addEventListener('click', function(e){
+
+})
+editItemBtn.addEventListener('click', function(e){
 
 })
